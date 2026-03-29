08a1e828556a78b4423f1f8b6b29c1e516e9bd55
wdev-jahidhasan <wdev-jahidhasan@gmail.com>
2026-03-29 21:31:28 +0600

    font, tailwind and daisyui added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-2xl font-bold text-green-500 text-center'>Test</h1>
+    <button className='btn btn-error'>Test</button>
+    </>
+  )
+}
+
+export default App
